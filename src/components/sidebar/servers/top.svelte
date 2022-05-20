8ea926dc5fe44a10b6676500fdf2b513f050e256
crizmo <crizmo595@gmail.com>
2022-05-20 12:24:41 +0530

    Create top.svelte

---
+++ src/components/sidebar/servers/top.svelte
@@ -0,0 +1,41 @@
+<script>
+    const serverWidth = "50px";
+</script>
+
+<main>
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <div class="sidebar-header-logo">
+                <img
+                    src="https://media.discordapp.net/attachments/952764969638834228/969830435444097054/Criz.png"
+                    alt=""
+                    width={serverWidth}
+                />
+            </div>
+            <hr>
+        </div>
+    </div>
+</main>
+
+<style>
+    
+    .sidebar-header-logo {
+        text-align: center;
+        padding-top: 10px
+    }
+
+    .sidebar-header-logo img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+
+
+    .sidebar-header-logo img:hover {
+        border-radius: 10%;
+        transition: all 0.3s ease-in-out;
+    }
+
+    
+</style>
