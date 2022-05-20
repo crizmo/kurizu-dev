072ab265e668ff3dd50980a83654aaa331779066
crizmo <crizmo595@gmail.com>
2022-05-20 18:34:56 +0530

    :balloon: projects

---
+++ src/components/content/projects/streamsavers.svelte
@@ -0,0 +1,139 @@
+<script>
+    const serverWidth = "50px";
+</script>
+
+<main>
+    <div class="mainarea">
+        <div class="top-nav">
+            <h3 class="channel-name"># StreamSavers</h3>
+            <div class="vl" />
+            <p class="channel-info">Free, open-source and easy-to-use service to loop stream 24/7 to YouTube </p>
+        </div>
+        <hr>
+        <div class="chat-body">
+            <div class="chat-body-messages">
+                <div class="chat-body-messages-item">
+                    <div class="chat-body-messages-item-avatar">
+                        <img
+                            src="https://media.discordapp.net/attachments/952764969638834228/969830435444097054/Criz.png"
+                            alt=""
+                            width={serverWidth}
+                        />
+                    </div>
+                    <div class="chat-body-messages-item-content">
+                        <div class="chat-body-messages-item-content-header">
+                            <h3 class="chat-body-messages-item-content-header-name">Kurizu</h3>
+                        </div>
+                        <div class="chat-body-messages-item-content-body">
+                            <p>
+                                Stream Savers is a free, open-source and easy-to-use service to loop stream 24/7 to YouTube <br>
+                                Me and my friend <a href="https://coding398.dev/">coding398</a> <br>
+                                It helped me learn a lot of new things during my time working on this project. <br>
+                                I hope you will enjoy it ! <br><br>
+
+                                Stream Savers Links :- <br>
+                                <a href="https://streamsavers.live/">Website</a> <br>
+                                <a href="https://github.com/crizmo/Stream-Savers">Github</a> <br> <br>
+
+                                Stream Savers : <br>
+                                <img src="https://cdn.discordapp.com/attachments/970974581944885268/977185629509136444/streamsavers.png" alt="homepgimg" height="300px" width="auto" style="border-radius: 10px;">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+    .mainarea {
+        position: absolute;
+        width: 80.4%;
+        height: 100%;
+        /* text-align: left; */
+        margin-left: 352px;
+        background-color: #2a2c2e;
+        top: 0%;
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        /* overflow: hidden; */
+        overflow: scroll;
+    }
+
+    .top-nav {
+        align-items: left;
+        text-align: left;
+        margin-left: 20px;
+        margin-top: 60px;
+        background-color: aqua;
+    }
+
+    .vl {
+        border-left: 2px solid gray;
+        height: 25px;
+        position: absolute;
+        left: 11%;
+        top: 1.7%;
+    }
+
+    .channel-name {
+        top: 0%;
+        left: 1%;
+        position: absolute;
+    }
+
+    .channel-info {
+        position: absolute;
+        top: 0.5%;
+        left: 14%;
+    }
+
+    .chat-body {
+        position: absolute;
+        width: 100%;
+        height: 93%;
+        background-color: #313233;
+        top: 6.2%;
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        overflow: scroll;
+    }
+
+    .chat-body-messages {
+        position: absolute;
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        top: 2%;
+    }
+    
+    .chat-body-messages-item-avatar {
+        margin-left: 20px;
+        position: absolute;
+        border-radius: 100px;
+        overflow: hidden;
+    }
+
+    .chat-body-messages-item-content {
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        /* text-align: left;
+        margin-left: 80px; */
+    }
+
+    .chat-body-messages-item-content-header {
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        text-align: left;
+        margin-left: 80px;
+    }
+
+    .chat-body-messages-item-content-body {
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        text-align: left;
+        margin-left: 80px;
+        margin-right: 500px;
+    }
+
+</style>
