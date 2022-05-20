5214d2bf5800424e7a2bbf5b56c9aaebd58d97fa
crizmo <crizmo595@gmail.com>
2022-05-20 18:34:35 +0530

    Update all-pro.svelte

--- src/components/content/projects/all-pro.svelte
+++ src/components/content/projects/all-pro.svelte
@@ -2,9 +2,13 @@
 	import {Route} from 'tinro'; 
 	import ElinaCn from '../../channels/projects/elina-cn.svelte';
 	import AnyanimeCn from '../../channels/projects/anyanime-cn.svelte';
+	import StreamSaversCn from '../../channels/projects/streamsavers-cn.svelte';
+	import PixitCn from '../../channels/projects/pixit-cn.svelte';
 
 	import Anyanime from '../projects/anyanime.svelte';
 	import ElinaDev from '../projects/elina-dev.svelte';
+	import StreamSavers from '../projects/streamsavers.svelte';
+	import Pixit from '../projects/pixit.svelte';
 </script>
 
 <main>
@@ -16,6 +20,16 @@
 	<Route path="/elina">
 		<ElinaCn />
 		<ElinaDev />
+	</Route>
+
+	<Route path="/stream-savers">
+		<StreamSaversCn />
+		<StreamSavers />
+	</Route>
+
+	<Route path="/pixit">
+		<PixitCn />
+		<Pixit />
 	</Route>
 </main>
 
