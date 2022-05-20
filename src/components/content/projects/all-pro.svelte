805221509f57b1bbf69927a215a08d52743624c9
crizmo <crizmo595@gmail.com>
2022-05-20 12:24:14 +0530

    updates project routes pos

---
+++ src/components/content/projects/all-pro.svelte
@@ -0,0 +1,24 @@
+<script>
+	import {Route} from 'tinro'; 
+	import ElinaCn from '../../channels/projects/elina-cn.svelte';
+	import AnyanimeCn from '../../channels/projects/anyanime-cn.svelte';
+
+	import Anyanime from '../projects/anyanime.svelte';
+	import ElinaDev from '../projects/elina-dev.svelte';
+</script>
+
+<main>
+	<Route path="/anyanime">
+		<AnyanimeCn />
+		<Anyanime />
+	</Route>
+
+	<Route path="/elina">
+		<ElinaCn />
+		<ElinaDev />
+	</Route>
+</main>
+
+<style>
+
+</style>
