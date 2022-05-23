190b62ba22ee49443a0688fc67b63d015511c09c
crizmo <crizmo595@gmail.com>
2022-05-23 13:59:36 +0530

    :balloon: : spacing

--- src/components/content/other/todo.svelte
+++ src/components/content/other/todo.svelte
@@ -18,7 +18,7 @@
             <h3 class="channel-name"># Kurizu's Page</h3>
             <div class="vl" />
             <p class="channel-info">Kurizu Home page</p>
-            <span class="open-btn" on:click="{openNav}">&#9776;</span>
+            <!-- <span class="open-btn" on:click="{openNav}">&#9776;</span> -->
         </div>
         <hr />
         <div class="chat-body">
@@ -104,15 +104,9 @@
         left: 14%;
     }
 
-    .open-btn {
-        /* position: absolute;
-        top: 1.5%;
-        left: 95%;
-        font-size: 25px;
-        cursor: pointer;
-        color: orangered; */
+    /* .open-btn {
         display: none;
-    }
+    } */
 
     .chat-body {
         position: absolute;
@@ -220,7 +214,7 @@
             left: 44%;
         }
 
-        .open-btn {
+        /* .open-btn {
             position: absolute;
             top: 1.7%;
             left: 80%;
@@ -228,7 +222,7 @@
             cursor: pointer;
             color: orangered;
             display: block;
-        }
+        } */
 
         /* .msg-img {
             width: 100%;
