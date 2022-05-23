cb1afd2bcccb6535021668017ba3f288bf8562b9
crizmo <crizmo595@gmail.com>
2022-05-23 15:27:59 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import {Route} from 'tinro'; 
+	import { MetaTags } from 'svelte-meta-tags';
 
 	import Sidebar from './components/sidebar/sidebar.svelte';
 
@@ -14,6 +15,34 @@
 	import About from './components/content/home/about.svelte';
 
 </script>
+
+<MetaTags
+  title="Kurizu"
+  titleTemplate="%s | Discord themed website"
+  description="Kurizu's website | Discord themed website"
+  canonical="https://www.canonical.ie/"
+  openGraph={{
+    url: 'https://kurizu.vercel.app/',
+    title: 'Kurizu',
+    description: 'Kurizu - Discord themed website',
+    images: [
+      {
+        url: 'https://cdn.discordapp.com/attachments/970974282681307187/977539362432643082/criz.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Og Image Alt'
+      },
+      {
+        url: 'https://cdn.discordapp.com/avatars/784141856426033233/2b71440eb154c1c2897e956f1f0da7b7.webp',
+        width: 900,
+        height: 800,
+        alt: 'Og Image Alt Second'
+      },
+      { url: 'https://cdn.discordapp.com/attachments/970974282681307187/978162022988525599/kurizu.jpg' },
+    ],
+    site_name: 'kurizu.vercel.app'
+  }}
+/>
 
 <main>
 	<Sidebar/>
