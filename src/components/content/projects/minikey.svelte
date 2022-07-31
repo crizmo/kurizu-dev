3488355ba456564916f3c18b9c0f70bd55ba0b34
crizmo <crizmo595@gmail.com>
2022-07-31 15:25:24 +0530

    minikey channel

---
+++ src/components/content/projects/minikey.svelte
@@ -0,0 +1,265 @@
+<script>
+    const serverWidth = "50px";
+    import User from '../../-comp/user.svelte'; 
+
+    import { closeNav, openNav } from "../../channels/home/home-cn.svelte";
+
+    import { swipe } from "svelte-gestures";
+    let direction;
+
+    function handler(event) {
+        direction = event.detail.direction;
+
+        
+
+        if (window.innerWidth < 1500) {
+            if (direction == "left") {
+                closeNav();
+            } else if (direction == "right") {
+                openNav();
+            }
+        }
+    }
+</script>
+
+<main>
+    <div class="mainarea" use:swipe={{ timeframe: 500, minSwipeDistance: 0.1 }} on:swipe={handler}>
+        <div class="top-nav">
+            <h3 class="channel-name"># Minikey</h3>
+            <div class="vl" />
+            <p class="channel-info">Compact Keryboard Design</p>
+            <!-- <span class="open-btn" on:click="{openNav}">&#9776;</span> -->
+        </div>
+        <hr>
+        <div class="chat-body">
+            <div class="chat-body-messages">
+                <div class="chat-body-messages-item">
+                    <User />
+                    <div class="chat-body-messages-item-content">
+                        <div class="chat-body-messages-item-content-header">
+                            <h3 class="chat-body-messages-item-content-header-name">Kurizu</h3>
+                        </div>
+                        <div class="chat-body-messages-item-content-body">
+                            <p>
+                                Minikey is a Compact Keyboard design with live user input made using svelte.
+                                Check it out if u want : <a href="https://mini-key.kurizu.repl.co/">https://mini-key.kurizu.repl.co/</a>
+                                <br><br>
+                                <img class="msg-img" src="https://cdn.discordapp.com/attachments/1003176063666491452/1003238076950249523/minikey.png" alt="homepgimg" height="300px" width="auto" style="border-radius: 10px;"> <br>
+                                <br>
+                                This is the base design of Minikey. <br>
+                                I plan on adding a 3d key look to the keyboard in the near future as well as a rgb effect <br>
+                                If you want to help me do it feel free to join me on the <a href="https://github.com/crizmo/Mini-Key">repo</a> <br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="chat-body-messages-item">
+                    <User />
+                    <div class="chat-body-messages-item-content">
+                        <div class="chat-body-messages-item-content-header">
+                            <h3 class="chat-body-messages-item-content-header-name">Kurizu</h3>
+                        </div>
+                        <div class="chat-body-messages-item-content-body">
+                            <p>
+                                Minikey project links : <br>
+                                <a href="https://github.com/crizmo/Mini-Key">Github</a> <br>
+                                <a href="https://replit.com/@kurizu/Mini-Key?v=1">Replit</a> <br>
+                                <a href="https://mini-key.kurizu.repl.co/">Website</a> <br>
+                                <br>
+                                Thank you for checking out Minikey! <br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+
+    main a {
+        color: #38b9ec;
+    }
+
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: #888; 
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: #555; 
+    }
+    
+    .mainarea {
+        position: absolute;
+        width: 81.25%;
+        height: 100%;
+        /* text-align: left; */
+        margin-left: 352px;
+        background-color: #282a2e;
+        top: 0%;
+        color: rgb(255, 255, 255);
+        z-index: 2;
+        /* overflow: hidden; */
+        overflow: fixed;
+    }
+
+    .top-nav {
+        align-items: left;
+        text-align: left;
+        margin-left: 20px;
+        margin-top: 60px;
+        background-color: aqua;
+    }
+
+    .vl {
+        border-left: 2px solid gray;
+        height: 25px;
+        position: absolute;
+        left: 11%;
+        top: 1.7%;
+    }
+
+    .channel-name {
+        top: 0%;
+        left: 1%;
+        position: absolute;
+    }
+
+    .channel-info {
+        position: absolute;
+        top: 0.5%;
+        left: 14%;
+    }
+
+    /* .open-btn {
+        display: none;
+    } */
+
+    .chat-body {
+        position: absolute;
+        width: 100%;
+        height: 93%;
+        background-color: #36393f;
+        top: 6.2%;
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        overflow: scroll;
+    }
+
+    .chat-body-messages {
+        position: absolute;
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        top: 2%;
+    }
+
+    .chat-body-messages-item-content {
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        /* text-align: left;
+        margin-left: 80px; */
+    }
+
+    .chat-body-messages-item-content-header {
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        text-align: left;
+        margin-left: 80px;
+    }
+
+    .chat-body-messages-item-content-body {
+        color: rgb(255, 255, 255);
+        z-index: 1;
+        text-align: left;
+        margin-left: 80px;
+        margin-right: 50px;
+    }
+
+    @media screen and (max-width: 1910px) {
+        .mainarea {
+            position: absolute;
+            width: 91.3%;
+            height: 100%;
+            /* text-align: left; */
+            margin-left: 315px;
+            top: 0%;
+            color: rgb(255, 255, 255);
+            z-index: 2;
+            overflow: scroll;
+        }
+    }
+
+    @media screen and (max-width: 1500px) {
+        .mainarea {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            /* text-align: left; */
+            margin-left: 55px;
+            top: 0%;
+            color: rgb(255, 255, 255);
+            z-index: 0;
+            overflow: scroll;
+
+            font-size: small;
+        }
+
+        .top-nav {
+            align-items: left;
+            text-align: left;
+            margin-left: 0px;
+            margin-top: 60px;
+            background-color: aqua;
+        }
+
+        .vl {
+            border-left: 2px solid gray;
+            height: 25px;
+            position: absolute;
+            left: 23%;
+            top: 1.7%;
+        }
+
+        .channel-name {
+            top: 0%;
+            left: 2%;
+            position: absolute;
+        }
+
+        .channel-info {
+            position: absolute;
+            top: 0.5%;
+            left: 25%;
+        }
+
+        /* .open-btn {
+            position: absolute;
+            top: 1.7%;
+            left: 80%;
+            font-size: 20px;
+            cursor: pointer;
+            color: orangered;
+            display: block;
+        } */
+
+        .msg-img {
+            width: 100%;
+            height: auto;
+            max-width: 300px;
+        }
+
+        .chat-body-messages-item-content-body {
+            color: rgb(255, 255, 255);
+            z-index: 1;
+            text-align: left;
+            margin-left: 80px;
+            margin-right: 70px;
+        }
+    }
+
+</style>
