dd94dff3ca6fc9582050a6f1215176425e93c54e
crizmo <crizmo595@gmail.com>
2022-09-06 23:53:42 +0530

    🔨 : hover

--- src/components/channels/categories/projects.svelte
+++ src/components/channels/categories/projects.svelte
@@ -26,6 +26,9 @@
                     <button class="channelbtn">
                         <a class="minikey-cn" href="/minikey"># Minikey</a>
                     </button>
+                    <button class="channelbtn">
+                        <a class="type3d-cn" href="/type3d"># Type3D</a>
+                    </button>
                 </div>
             </details>
         </div>
@@ -80,7 +83,8 @@
     .stream-savers-cn:hover,
     .pixit-cn:hover,
     .breeze-cn:hover,
-    .minikey-cn:hover {
+    .minikey-cn:hover,
+    .type3d-cn:hover {
         background: rgb(104, 104, 104) none repeat scroll 0 0;
         border-right: 5px none white;
         box-sizing: border-box;
