132233e2132ca234b169b67c9360ba997198a139
Kurizu <83665497+crizmo@users.noreply.github.com>
2022-10-30 15:02:02 +0530

    Update chat.svelte

--- kurizu/src/components/content/chat/chat.svelte
+++ kurizu/src/components/content/chat/chat.svelte
@@ -21,7 +21,7 @@
 
     import io from "socket.io-client";
 
-    const socket = io("http://localhost:3000");
+    const socket = io("https://dev-kurizu.kurizu.repl.co");
 
     let messages = [];
     let message = "";
