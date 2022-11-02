ea1178ccd50f6473564dc0f5c218c1c099580432
crizmo <crizmo595@gmail.com>
2022-11-02 18:43:14 +0530

    🗼 : chat backup

--- dev-server/server.js
+++ dev-server/server.js
@@ -1,18 +1,7 @@
 const { Server } = require("socket.io")
-require("dotenv").config()
+const fs = require("fs")
 
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-// mongoose.connect(process.env.mongodb);
-
-// const userSchema = new Schema({
-//   name: String,
-//   pfp: String,
-//   msg: String,
-// });
-
-let msgs = [];
-
+let msgs = require("./msg.json") || [];
 const io = new Server({
   cors: {
     origin: "*"
@@ -20,12 +9,16 @@ const io = new Server({
 })
 
 io.on('connection', (socket) => {
-  socket.emit('previous messages', msgs)
+  socket.emit('previous messages', require("./msg.json") || []);
   socket.on('chat message', (usr, pfp, msg) => {
-
+    
     let data = [usr,pfp,msg]
     msgs.push(data);
-    if (msgs.length == 11) msgs.splice(0, 1);
+    if (msgs.length == 11) msgs.splice(0, 1); 
+
+    fs.writeFileSync('./msg.json', JSON.stringify(msgs), (err) => { 
+      if (err) throw err;
+    }); 
 
     io.emit('chat message', usr, pfp, msg);
   });
